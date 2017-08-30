97996d5cdc7d4c4cf5ce9596ccf8ae67238fbadc
chiujef <31404684+chiujef@users.noreply.github.com>
2017-08-30 12:23:43 +1200

    Added Services Export
    Added the services to the barrel.

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,3 +1,7 @@
 /* Models Export */
 export { Note } from './models/note';
 export { Notebook } from './models/notebook';
+
+/* Services Export */
+export { NoteService } from './services/note.service';
+export { NotebookService } from './services/notebook.service';
